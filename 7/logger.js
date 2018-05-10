69552792eda9b45e8160220acd4341f460c0b4a1
kinggggg <liweibooooo@126.com>
2018-05-10 15:35:06 +0800

    connect更新了logger更换成了morgan; 测试logger

--- 7/logger.js
+++ 7/logger.js
@@ -1,5 +1,8 @@
+var http = require('http');
 var connect = require('connect');
+var logger = require('morgan');
 
 var app = connect()
-          .use(connect.logger())
+// .use(connect.logger()) //connect更新了 logger更换成了morgan。不能这么用，应该为下面一行
+          .use(logger('dev'))
           .listen(3000);
